004a196c7c9337aee88d7962ad6aa5b473dcdff6
pratik1096 <deshmukhprat@gmail.com>
2020-09-28 13:50:35 +0530

    commit3

---
+++ LM_time.ino
@@ -0,0 +1,153 @@
+#ifdef ESP32
+#include <WiFi.h>
+#include <HTTPClient.h>
+#else
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#endif
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+WiFiClient espClient;
+PubSubClient client(espClient);
+long now = millis();
+long lastMeasure = 0;
+#ifdef ESP32
+#include <WiFi.h>
+#include <HTTPClient.h>
+#else
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#endif
+const char* ssid = "AUKCSC_SMARTOFFICE";
+const char* password = "PASSWORD2020";
+String payload;
+const char* host = "http://192.168.1.1/gettime.php";
+const char* serverName = "http://192.168.1.1/post-esp-data.php";
+const char* cloudserverName = "http://52.237.118.211:8000/iotdevicedata/LM";
+String apiKeyValue = "tPmAT5Ab3j7F9";
+String productcode = "AukproDevice11029";
+String uniquekey = "!AiOtAtAuKpRoLm01";
+String modelname = "LM";
+int outputpin= A0;
+float millivolts;
+int analogValue;
+float celsius;
+int p1;
+String date;
+int p2;
+String currenttime;
+int p3;
+String hh;
+int p4;
+String mm;
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(outputpin,INPUT);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void gettime() {
+  HTTPClient http;
+  http.begin(host);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    payload = http.getString();
+    // Serial.println(payload);
+    p1 = payload.indexOf('&');
+    date = payload.substring(0, p1);
+    p2 = payload.indexOf('&', p1 + 1 );
+    currenttime = payload.substring(p1 + 1, p2 + 1);
+    Serial.println(date);
+    currenttime.remove(8);
+    //  Serial.println(fuel);
+    p3 = currenttime.indexOf(':');
+    hh = currenttime.substring(0, p3);
+    Serial.println(hh);
+    p4 = currenttime.indexOf(':', p3 + 1 );
+    mm = currenttime.substring(p3 + 1, p4 + 1);
+    mm.remove(2);
+    Serial.println(mm);
+  }
+  http.end();
+  delay(1000);
+}
+void server() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String httpRequestData =  "api_key=" + apiKeyValue + "&product_code=" + productcode + "&model_name=" + modelname + "&unique_key=" + uniquekey + "&temperature=" +  celsius +  "" ;
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    int httpResponseCode = http.POST(httpRequestData);
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(2000);
+}
+
+void cloudserver() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(cloudserverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    //    light=analogRead(ldr);
+    String httpRequestData =   "api_key=" + apiKeyValue + "&product_code=" + productcode + "&model_name=" + modelname + "&unique_key=" + uniquekey +
+                               "&temperature=" + celsius + "&date=" +  date +  "&hh=" + hh +  "&mm=" + mm + "" ;
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    int httpResponseCode = http.POST(httpRequestData);
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(2000);
+}
+
+void loop()
+{
+  analogValue = analogRead(outputpin);
+  millivolts = (analogValue / 1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+  celsius = millivolts / 10;
+  Serial.print("in DegreeC=   ");
+  Serial.println(celsius);
+  gettime();
+  server();
+  cloudserver();
+
+}
